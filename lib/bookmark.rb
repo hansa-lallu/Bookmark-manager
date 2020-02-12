d623f46827e79a4fa97b611abce7d255a75489db
Peter <pc.dean@icloud.com>
2020-02-12 15:54:48 +0000

    removed inncorect comment

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,7 +1,8 @@
 require 'pg'
 class Bookmark
-#self is a new instance of a class
+
   def self.all
+    
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
@@ -11,4 +12,5 @@ class Bookmark
     result = connection.exec("SELECT * FROM bookmarks")
     result.map { |bookmark| bookmark['url'] }
   end
+
 end
